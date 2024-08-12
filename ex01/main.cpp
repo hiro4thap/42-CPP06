98e101201f9e8148c71f466132e3eed985b375cf
Hiroaki Ono <hiono@student.42adel.org.au>
2024-08-12 15:55:27 +0930

    chech class cannot be instantized

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -3,15 +3,17 @@
 
 int	main(void)
 {
+	//Serializer s;	// -> compile error;
+
 	Data data;
 	data.member = 10;
-	std::cout << "original address: " << &data << "\n";
+	std::cout << "data address:   " << &data << "\n";
 
 	Log::nl("serialize...", YELLOW);
 	uintptr_t raw = Serializer::serialize(&data);
 	Log::nl("deserialize...", YELLOW);
 	Data *res = Serializer::deserialize(raw);
-	std::cout << "result address:   " << res << "\n";
+	std::cout << "result address: " << res << "\n";
 
 	return 0;
 }
